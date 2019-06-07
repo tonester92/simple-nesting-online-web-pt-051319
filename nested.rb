bca5cdef68bfa887171db39873f83ef6de7b81ae
Antonio Blandin <antonio.blandin25@gmail.com>
2019-06-07 22:08:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -80,7 +80,7 @@ def adding_matz
   programmer_hash[:yukihiro_matsumoto] = {
     known_for: "Ruby",
     languages:
-      ["Inheritance", "C"]
+      ["Abstraction", "C"]
     }
   programmer_hash
 end
